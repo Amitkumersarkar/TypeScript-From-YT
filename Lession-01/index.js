701f1ee55940fe8b8d47a8825dc8e3acc303fab0
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-11-14 12:55:43 +0600

    added lession 01 task

---
+++ Lession-01/index.js
@@ -0,0 +1,8 @@
+var userName = "Amit Sarkar";
+var age = 25;
+var profession = "Student";
+var dept = "CSE";
+console.log(userName);
+console.log(age);
+console.log(profession);
+console.log(dept);
---
+++ Lession-01/task1.js
@@ -0,0 +1,5 @@
+function addNum(num1, num2) {
+    console.log(num1 + num2);
+}
+addNum(10, 20);
+// addNum(10, "20");
