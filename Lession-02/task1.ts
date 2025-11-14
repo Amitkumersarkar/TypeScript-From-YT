a931602be3e2ffc368bb17689f4c21aedeb3434a
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-11-14 15:13:50 +0600

    added lession 02 file

---
+++ Lession-02/task1.ts
@@ -0,0 +1,11 @@
+// built-in data type: number,string,boolean,void,undefined,null
+let userId: number;
+// userId = 123;
+let UserName: string;
+let isActivated: boolean;
+
+userId = 101;
+UserName = "Amit";
+isActivated = true;
+
+console.log(`userId :${userId},`, `UserName:${UserName},`, `isActivated:${isActivated},`);
