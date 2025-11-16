83d2c81ca9a26b3381386419cdc0ea93fd1d22c7
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-11-16 21:18:26 +0600

    union type in ts

---
+++ Lession-03/union-type.js
@@ -0,0 +1,11 @@
+var userId;
+userId = "101";
+userId = 101;
+userId = true;
+// function with typed parameter
+function displayUserInfo(userId) {
+    console.log(userId);
+}
+displayUserInfo("101");
+displayUserInfo(101);
+displayUserInfo(true);
