596815af425ae3b37b8196e437cf00d872c83d0e
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-11-18 00:05:16 +0600

    added enum data type

---
+++ Lession-04/stringEnum.js
@@ -0,0 +1,7 @@
+var UserType;
+(function (UserType) {
+    UserType["readData"] = "REDA_DATA";
+    UserType["deleteData"] = "REDA_DATA";
+})(UserType || (UserType = {}));
+console.log(UserType.readData);
+console.log(UserType['readData']);
--- Lession-04/stringEnum.ts
+++ Lession-04/stringEnum.ts
@@ -1,5 +1,8 @@
+// hetergenous enum
 enum UserType {
-    readData = "REDA_DATA"
-    deleteData = "REDA_DATA"
+    readData = "REDA_DATA",
+    deleteData = "REDA_DATA",
+    id = 101
 }
 console.log(UserType.readData)
+console.log(UserType['readData']);
