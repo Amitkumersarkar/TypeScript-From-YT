f2d3973fcef7ea3bfa6589e155badc16038a35a3
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-11-21 21:40:20 +0600

    another custom type method

---
+++ Lession-05/customType2.ts
@@ -0,0 +1,12 @@
+type User = {
+    userName: string;
+    userId: number;
+};
+
+let users: User[] = [];
+
+users.push({ userName: 'Amit', userId: 283 });
+users.push({ userName: 'Niloy', userId: 285 });
+users.push({ userName: 'Anik', userId: 288 });
+
+console.log(users);
