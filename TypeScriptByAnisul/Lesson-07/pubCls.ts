7c8d0fe9826473a42f7d2aab682e15e3f0e0ad60
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-11-24 23:38:02 +0600

    added publc class in ts

---
+++ TypeScriptByAnisul/Lesson-07/pubCls.ts
@@ -0,0 +1,33 @@
+class User {
+    public userName: string;
+    public age: number;
+
+    constructor(userName: string, age: number) {
+        this.userName = userName;
+        this.age = age;
+    }
+
+    display(): void {
+        console.log("Hello");
+    }
+}
+
+class Student extends User {
+    studentId: number;
+
+    constructor(userName: string, age: number, studentId: number) {
+        super(userName, age);
+        this.studentId = studentId;
+    }
+
+    display(): void {
+        console.log(`userName: ${this.userName}, age: ${this.age}, id: ${this.studentId}`);
+    }
+}
+
+let student1 = new Student("Amit", 23, 22235103283);
+student1.display();
+
+let user1 = new Student("Niloy", 22, 22235103285);
+user1.userName = "Rupak";
+console.log(user1);
